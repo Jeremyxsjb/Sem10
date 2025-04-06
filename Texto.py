970fb26b91b52ce32235abcbf3d7f385dda7c9f0
Jeremyxsjb <stevenjeremy200324@gmail.com>
2025-04-06 17:01:22 -0500

    Add files via upload

---
+++ Texto.py
@@ -0,0 +1,5 @@
+archivo = open('My_Notes.txt', 'r') #abrimos el archivo
+print (archivo.read()) #devuleve el contenido como dadena de texto
+#print(archivo.readlines()) #Lee linea por linea
+archivo.close() #cerramos el archivo
+
